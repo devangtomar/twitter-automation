d876148b2345b67dd3757135824182239adc0aba
Devang Tomar <devangtomar123@gmail.com>
2023-05-05 16:46:40 +0530

    Adding getFollower() function

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ const T = new Twit({
     access_token: 'YOUR_ACCESS_TOKEN',
     access_token_secret: 'YOUR_ACCESS_TOKEN_SECRET',
 });
+
+// Fetch the user's followers
+function getFollowers(screenName) {
+    return new Promise((resolve, reject) => {
+        T.get('followers/list', { screen_name: screenName }, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                const followers = data.users.map((user) => ({
+                    id: user.id,
+                    screen_name: user.screen_name,
+                }));
+                resolve(followers);
+            }
+        });
+    });
+}
