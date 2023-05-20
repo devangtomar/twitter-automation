a1651abb507e9c8a2a042121794d07cee744a954
Devang Tomar <32426600+devangtomar@users.noreply.github.com>
2023-05-20 16:45:47 +0530

    ➕ First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const Twit = require('twit');
+
+// Set up your Twitter API credentials
+const T = new Twit({
+    consumer_key: 'YOUR_CONSUMER_KEY',
+    consumer_secret: 'YOUR_CONSUMER_SECRET',
+    access_token: 'YOUR_ACCESS_TOKEN',
+    access_token_secret: 'YOUR_ACCESS_TOKEN_SECRET',
+});
